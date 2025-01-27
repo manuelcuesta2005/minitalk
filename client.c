431ccb73980b2cbdba9ae23a4471d2f08092dbd9
macuba2005 <107274670+manuelcuesta2005@users.noreply.github.com>
2025-01-27 14:43:28 -0500

    minitalk configured

--- client.c
+++ client.c
@@ -9,10 +9,10 @@
 /*   Updated: 2024/12/16 20:18:19 by mcuesta-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <signal.h>
-#include <unistd.h>
 #include "../ft_printf/ft_printf.h"
 #include "../libft/libft.h"
+#include <signal.h>
+#include <unistd.h>
 
 void	send_messages(int pid_server, char *message)
 {
@@ -27,12 +27,12 @@ void	send_messages(int pid_server, char *message)
 		bit = 0;
 		while (bit < 8)
 		{
-			if((letter >> bit) & 1)
+			if (letter >> (7 - bit) & 1)
 				kill(pid_server, SIGUSR1);
 			else
 				kill(pid_server, SIGUSR2);
 			bit++;
-			usleep(5000);
+			usleep(100);
 		}
 		i++;
 	}
@@ -40,27 +40,27 @@ void	send_messages(int pid_server, char *message)
 	while (bit < 8)
 	{
 		kill(pid_server, SIGUSR2);
-		usleep(5000);
+		usleep(100);
 		bit++;
 	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
+	int	pid_server;
+
 	if (argc != 3)
 	{
-		ft_printf("Uso del cliente: %s <PID> <mensaje para el servidor> \n", argv[0]);
+		ft_printf("Uso del cliente: %s <PID> <mensaje para el servidor> \n",
+			argv[0]);
 		return (1);
 	}
-	int	pid_server;
-	
 	pid_server = ft_atoi(argv[1]);
 	if (pid_server <= 0)
 	{
 		ft_printf("Error: PID del servidor invalido. \n");
 		return (1);
 	}
-
 	send_messages(pid_server, argv[2]);
 	ft_printf("Mensaje enviado con exito \n");
 	return (0);
